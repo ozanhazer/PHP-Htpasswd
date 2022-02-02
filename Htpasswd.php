76325a00c9bb987ad9d293be0302ae2b0d66dcbe
M.Ozan Hazer <ozanhazer@gmail.com>
2022-02-02 12:42:45 +0300

    Support php8+

--- Htpasswd.php
+++ Htpasswd.php
@@ -132,7 +132,7 @@ class Htpasswd
             $text .= substr($bin, 0, min(16, $i));
         }
         for ($i = $len; $i > 0; $i >>= 1) {
-            $text .= ($i & 1) ? chr(0) : $plainpasswd{0};
+            $text .= ($i & 1) ? chr(0) : $plainpasswd[0];
         }
         $bin = pack("H32", md5($text));
         for ($i = 0; $i < 1000; $i++) {
